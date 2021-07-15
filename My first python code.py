e6474a6776f6453d9cc9ffc5fc6f4c7b53637edf
sudheeshkumarpythonista <81633927+sudheeshkumarpythonista@users.noreply.github.com>
2021-07-15 17:53:23 +0530

    Update My first python code.py

--- My first python code.py
+++ My first python code.py
@@ -1,7 +1,7 @@
 import datetime
 x = datetime.datetime.today() - datetime.timedelta(days=1)
 date = str((x.strftime("%b %d")))
-file = open("test.txt", "r")
+file = open("catalina.out", "r")
 for line in file:
     if date in line:
         print(line, next(file), end="")
